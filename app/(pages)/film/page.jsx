b1662ccfb952258d8803970dc6d60d7fa3d72e84
BarancanS <barancansng@gmail.com>
2023-07-20 16:46:32 +0300

    Update

--- app/(pages)/film/page.jsx
+++ app/(pages)/film/page.jsx
@@ -14,17 +14,14 @@ const Movies = () => {
   const [filteredPosts, SetFilteredPosts] = useState(posts);
   const [lowestRange, SetLowestRange] = useState(0);
   const [highestRange, SetHighestRange] = useState(10);
+  const [optionValue, SetOptionValue] = useState();
 
-  function FilterNetflixButtonClick() {
+  function FilterAllButtonClick() {
     SetFilteredPosts(
-      posts.filter((items) => items.platform.includes("Netflix"))
+      posts.filter((items) => items.platform.includes(`${optionValue}`))
     );
   }
-  function FilterAmazonButtonClick() {
-    SetFilteredPosts(
-      posts.filter((items) => items.platform.includes("Amazon"))
-    );
-  }
+
   function ClearFilterButtonClick() {
     SetFilteredPosts(posts);
   }
@@ -37,19 +34,44 @@ const Movies = () => {
     );
   }
   useEffect(() => {
+    SetFilteredPosts(posts);
     if (session.status === "unauthenticated") {
       setAuthSession(false);
     } else {
       setAuthSession(true);
     }
-  });
+  }, [session]);
+
   return authSession ? (
     <div>
       <Navbar />
       <SearchMovies />
       <div className="flex flex-col sm:flex sm:flex-row">
-        <div className="flex flex-col items-center mx-auto w-1/6 justify-start text-xl mt-5">
-          <button
+        <div className="flex flex-col items-center mx-auto w-1/6 justify-start text-xl mt-5 border-2 ml-2 h-2/5 rounded-xl">
+          <div className="flex flex-col items-center justify-center p-5">
+            <select
+              name=""
+              onChange={(e) => SetOptionValue(e.target.value)}
+              className="bg-black border-2"
+            >
+              <option></option>
+              <option value="Netflix">Netflix</option>
+              <option value="Amazon">Amazon</option>
+              <option value="YouTube">YouTube</option>
+            </select>
+            <button
+              onClick={FilterAllButtonClick}
+              className="p-1 mr-2 mt-2 border-2 rounded-2xl border-sky-100"
+            >
+              FilterAll
+            </button>
+            <button
+              onClick={ClearFilterButtonClick}
+              className="p-1 mr-2 mt-2 border-2 rounded-2xl border-sky-100"
+            >
+              Clear
+            </button>
+            {/* <button
             onClick={FilterNetflixButtonClick}
             className="p-1 mr-2 mt-2 border-2 rounded-2xl border-sky-100"
           >
@@ -72,38 +94,39 @@ const Movies = () => {
             className="p-1 mr-2 mt-2 border-2 rounded-2xl border-sky-100"
           >
             Imdb
-          </button>
-          <input
-            type="range"
-            id="vol"
-            name="vol"
-            min="0"
-            max="10"
-            value={lowestRange}
-            step={0.1}
-            onChange={(e) => SetLowestRange(e.target.value)}
-          />
-          <p>{lowestRange}</p>
-          <input
-            type="range"
-            id="vol"
-            name="vol"
-            min="0"
-            max="10"
-            value={highestRange}
-            step={0.1}
-            onChange={(e) => SetHighestRange(e.target.value)}
-          />
-          <p>{highestRange}</p>
+          </button> */}
+            <input
+              type="range"
+              id="vol"
+              name="vol"
+              min="0"
+              max="10"
+              value={lowestRange}
+              step={0.1}
+              onChange={(e) => SetLowestRange(e.target.value)}
+            />
+            <p>{lowestRange}</p>
+            <input
+              type="range"
+              id="vol"
+              name="vol"
+              min="0"
+              max="10"
+              value={highestRange}
+              step={0.1}
+              onChange={(e) => SetHighestRange(e.target.value)}
+            />
+            <p>{highestRange}</p>
+          </div>
         </div>
-        <div className="w-9/12 min-h-[calc(100vh-15rem)] mx-auto">
-          <div className="grid grid-cols-5  max-2xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-2 max-sm:grid-cols-1 justify-items-center mt-10">
+        <div className="w-full">
+          <div className="grid grid-cols-5  max-2xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-2 max-sm:grid-cols-2 justify-items-center mt-10">
             {filteredPosts.map((items, index) => {
               return (
                 <div key={index}>
                   <div
                     style={{ backgroundImage: `url("${items.imageUrl}")` }}
-                    className="max-sm:w-32 max-sm:h-44 w-60 h-80 bg-cover bg-no-repeat bg-center rounded-xl hover:scale-105 transition-all duration-700 ease-in-out"
+                    className="max-sm:w-28 max-sm:h-40 w-60 h-80 bg-cover bg-no-repeat bg-center rounded-xl hover:scale-105 transition-all duration-700 ease-in-out"
                   ></div>
                   <h1 className="text-center mt-10">{items.title}</h1>
                 </div>
