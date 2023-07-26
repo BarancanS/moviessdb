91fa704c5eca5c62655fcba0d68e7b7a0e3c7e9b
BarancanS <barancansng@gmail.com>
2023-07-27 01:38:00 +0300

    Update Films.jsx

--- app/components/Films.jsx
+++ app/components/Films.jsx
@@ -10,7 +10,7 @@ const Films = () => {
     <section className="flex flex-row mt-5 ">
       <div className="2xl:w-9/12 w-full">
         <h1 className="mb-3 text-3xl text-left">Trend Movies</h1>
-        <div className="w-11/12 flex flex-row overflow-y-scroll items-start pl-10 py-10 mx-auto  h-fit gap-5">
+        <div className="w-11/12 flex flex-row overflow-y-scroll items-start py-10 mx-auto pl-2  h-fit gap-5">
           {posts.slice(0, 10).map((items, index) => {
             return (
               <div key={index}>
