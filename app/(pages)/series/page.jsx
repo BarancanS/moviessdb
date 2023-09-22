dca0074fbe31856ea6bab67b5f26a1383778ae4a
BarancanS <barancansng@gmail.com>
2023-09-22 05:15:42 +0300

    Update page.jsx

--- app/(pages)/series/page.jsx
+++ app/(pages)/series/page.jsx
@@ -263,7 +263,7 @@ const Series = () => {
           <SignUp />
           <button
             onClick={handleStatusChange}
-            className="bg-blue-500 hover:bg-blue-600 text-white mt-20 py-2 px-4 rounded-md font-bold flex items-center justify-center"
+            className="bg-blue-500 hover:bg-blue-600 text-white mt-1 py-2 px-4 rounded-md font-bold flex items-center justify-center"
           >
             Login Page
           </button>
