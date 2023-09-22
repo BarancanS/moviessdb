448986422417fde3797a50c5e9ed8bf435627232
BarancanS <barancansng@gmail.com>
2023-09-23 00:03:20 +0300

    Update page.jsx

--- app/(pages)/categories/page.jsx
+++ app/(pages)/categories/page.jsx
@@ -61,18 +61,12 @@ const Categories = () => {
                             </div>
                           ))}
                         <Link href={`/${checkPage}/${items.id}`} key={index}>
-                          <div className="relative z-10 pl-4 hover:scale-105 transition-all duration-300 ease-in-out">
-                            <div
-                              style={{
-                                backgroundImage: `url("${`https://image.tmdb.org/t/p/original${items.poster_path}`}")`,
-                              }}
-                              className="rounded-lg p-2  shadow-xl hover:shadow-[10px_1px_50px_5px_rgba(0,0,0,0.3)] hover:shadow-fuchsia-800 shadow-fuchsia-950 transition-all duration-300 ease-in-out max-sm:w-28 max-[640px]:w-40 max-[330px]:w-28 max-sm:h-40 w-60 h-80 bg-cover bg-no-repeat bg-center"
-                            ></div>
-                            <h1 className="flex flex-row items-start justify-center h-20  max-[640px]:w-40 max-[330px]:w-28 max-sm:w-28 w-60 mt-5 max-md:text-sm text-white font-extrabold overflow-hidden">
-                              {items.name || items.title}
-                            </h1>
-                            <h1 className="text-left">{items.year}</h1>
-                          </div>
+                          <div
+                            style={{
+                              backgroundImage: `url("${`https://image.tmdb.org/t/p/original${items.poster_path}`}")`,
+                            }}
+                            className="max-sm:w-28 max-sm:h-40 w-60 h-80 bg-cover bg-no-repeat bg-center rounded-xl hover:scale-105 transition-all duration-300 ease-in-out"
+                          ></div>
                         </Link>
                       </div>
                     );
